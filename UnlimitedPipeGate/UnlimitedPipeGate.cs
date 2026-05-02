587790903c0b7791ef09ccc29b513fde24a60c5d
Aaronstar-McClure <jfeldt19@gmail.com>
2026-05-01 21:52:14 -0700

    boilerplate upload

---
+++ UnlimitedPipeGate/UnlimitedPipeGate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ILogger = Core.Logging.ILogger;
+using ShapezShifter.Hijack;
+using ShapezShifter.Flow;
+
+namespace UnlimitedPipeGate
+{
+
+    public class UnlimitedPipeGate : IMod
+    {
+        public UnlimitedPipeGate(ILogger logger)
+        {
+            AdjustValve();
+            logger.Info?.Log("Unlimited Pipe Gates!");
+        }
+
+        public void Dispose()
+        {
+        }
+
+        private void AdjustValve()
+        {
+            // PipeGateMetaBuildingDefinition
+            // ShapezShifter.Hijack.IBuildingsRewirer
+            // ShapezShifter.Hijack.IBuildingModulesRewirer
+            // IBuildingsRewirer.ModifyGameBuildings()
+            // BuildingDefinitionId definitionId = GameBuildings.PipeGateBuildingId;
+        }
+
+        private void AdjustValveAlt()
+        {
+            //IBuildingModulesRewirer.
+            //PipeGateBuildingModuleDataProvider
+            
+        }
+    }
+}
